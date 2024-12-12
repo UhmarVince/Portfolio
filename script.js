a89ab34fe55bcf2f0cdc6d36560098c0ced21e51
Uhmar Vince Aceron <aceronuhmarvince@gmail.com>
2024-12-13 02:51:17 +0800

    Main update

---
+++ script.js
@@ -0,0 +1,9 @@
+function str(){
+    document.getElementById("uhmar").style.color="rgb(26, 26, 67)";
+}
+function piting(){
+    document.getElementById("piting").style.color="rgb(26, 26, 67)";
+}
+function skills(){
+    document.getElementById("skills").style.color="rgb(26, 26, 67)";
+}
